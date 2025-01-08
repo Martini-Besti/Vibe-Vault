496aa0d6e2ae097804daf168e0491bc82a22728b
Martina Best <martinabest@Martinas-MacBook-Pro.local>
2025-01-08 15:51:21 +0000

    Push-URL-To-Vault

--- vibe-vault/src/components/TodoItem.js
+++ vibe-vault/src/components/TodoItem.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
-const TodoItem = ({title, description, completed, handleEdit}) => {
+const TodoItem = ({title, description, completed, handleEdit, photo}) => {
   return (
     <div className={`shadow-lg rounded-lg p-10 text-black ${
         completed ? "bg-green-300" : "bg-red-300"
     }`}>
         <h1>{title}</h1>
+        <img src={photo}></img>
         <p>{description}</p>
         <button onClick={handleEdit}>
             Mark as {completed ? "incomplete" : "complete"}
