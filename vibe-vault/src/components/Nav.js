240e5ef00e3a4acae9fad0f9d80b016e7631ae88
Martina Best <martinabest@Martinas-MacBook-Pro.local>
2025-01-08 14:29:41 +0000

    Amended-Home-Page

--- vibe-vault/src/components/Nav.js
+++ vibe-vault/src/components/Nav.js
@@ -3,16 +3,16 @@ import React from 'react'
 
 const nav = () => {
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-teal-900 p-6">
-<div className="mr-6 text-white p-6 bg-slate-600">
-TodoList
+    <nav className="flex items-center justify-between flex-wrap bg-[#1282A2] p-6">
+<div className="mr-6 text-[#F4FFFD] p-6 bg-slate-600">
+Vibe Vault
 </div>
 <div className="w-full flex items-center justify-end">
-    <Link href="/add" className="text-teal-200">
-    Add todo
+    <Link href="/add" className="text-[#F4FFFD]">
+    Add Photo &nbsp;
     </Link>
-    <Link href="/" className="text-teal-200">
-    View todos
+    <Link href="/" className="text-[#F4FFFD]">
+    My Vault
     </Link>
 
 </div>
