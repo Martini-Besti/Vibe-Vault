30fdbdcbdb768dd2e5d610d6ef2c289517673887
Martina Best <martinabest@Martinas-MacBook-Pro.local>
2025-01-09 15:59:28 +0000

    set-max-size-photos

--- vibe-vault/src/components/PhotoUpload.js
+++ vibe-vault/src/components/PhotoUpload.js
@@ -1,18 +1,20 @@
-import React from 'react'
+import React from "react";
 
-const PhotoUpload = ({title, description, completed, handleEdit, photo}) => {
+const PhotoUpload = ({ title, description, completed, handleEdit, photo }) => {
   return (
-    <div className={`shadow-lg rounded-lg p-10 text-black ${
-        completed ? "bg-green-400" : "bg-white"
-    }`}>
-        <h1>{title}</h1>
-        <img src={photo}></img>
-        <p>{description}</p>
-        <button onClick={handleEdit}>
-            Mark as {completed ? "plain" : "highlighted"}
-        </button>
+    <div
+      className={`shadow-lg rounded-lg p-10 text-black ${
+        completed ? "bg-yellow-200" : "bg-white"
+      }`}
+    >
+      <h1>{title}</h1>
+      <img src={photo}></img>
+      <p>{description}</p>
+      <button onClick={handleEdit}>
+        {completed ? "un-favourite" : "favourite"}
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default PhotoUpload
+export default PhotoUpload;
